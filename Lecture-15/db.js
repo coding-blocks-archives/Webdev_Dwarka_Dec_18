3b026828f6121cb0ec97ee917adf486dc1439e54
aayusharora <aayush113002@gmail.com>
2019-02-10 16:28:13 +0530

    Added db

--- Lecture-15/db.js
+++ Lecture-15/db.js
@@ -1,13 +1,25 @@
 const mongodb = require('mongodb').MongoClient;
 let dbuser = 'aayush';
 let dbpassword = 'aayush123';
+let dbname = 'shopcart'
 let url = `mongodb://${dbuser}:${dbpassword}@ds021166.mlab.com:21166/shopcart`;
-
+let collection = '';
 function connect() {
     mongodb.connect(url, function(err, client){
         if(err) throw err;
-        console.log(client);
+        let db = client.db(dbname);
+        collection = db.collection('shoppingcart');
+        insertDoc();
+        //console.log(db);
     })
+}
+
+function insertDoc(){
+   collection.insertMany([{a:1} ],function(err, result){
+       if (err) throw err;
+       console.log(result); 
+   })
+
 }
 
 module.exports = {
